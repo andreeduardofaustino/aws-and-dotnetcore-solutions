1100abe48de23dcfdb63a15ddbad24307be7e803
Andre Eduardo Faustino <aefaustino@gmail.com>
2023-05-03 20:26:10 -0300

    initial commit

---
+++ AWSSolution/SESWebApiCore/Controllers/SESController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
+
+namespace SESWebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SESController : ControllerBase
+    {
+        private readonly IAmazonSimpleEmailService _amazonSimpleEmailService;
+        private string _fromAddress = "aefaustino.aws22@gmail.com";
+        private string _toAddress = "aefaustino@gmail.com";
+        private string _subject = "SES";
+        private string _body = "<h1>It Worked!</h1> <p>So Happy!</p>";
+
+        public SESController(IAmazonSimpleEmailService amazonSimpleEmailService)
+        {
+            _amazonSimpleEmailService = amazonSimpleEmailService;
+        }
+
+        public async Task<ActionResult> SendEmail()
+        {
+            SendEmailRequest sendEmailRequest = new SendEmailRequest()
+            {
+                Destination = new Destination() { ToAddresses = new List<string>() { _toAddress } },
+                Source = _fromAddress,
+                Message = new Message()
+                {
+                    Subject = new Content()
+                    {
+                        Charset = "UTF-8",
+                        Data = _subject
+                    },
+                    Body = new Body()
+                    {
+                        Html = new Content()
+                        {
+                            Charset = "UTF-8",
+                            Data = _body
+                        }
+                    }
+                }
+            };
+
+            var sendResult =
+                await _amazonSimpleEmailService.SendEmailAsync(sendEmailRequest);
+
+            if (sendResult.HttpStatusCode != System.Net.HttpStatusCode.OK)
+            {
+                return BadRequest("Something went wrong!");
+            }
+
+            return Ok("Hi");
+        }
+    }
+}
