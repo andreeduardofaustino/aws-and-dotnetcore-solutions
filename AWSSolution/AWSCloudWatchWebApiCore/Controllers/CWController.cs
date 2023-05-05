50612512223aad17cf220ea0287348df04671f5a
Andre Eduardo Faustino <aefaustino@gmail.com>
2023-05-05 20:08:33 -0300

    AWS CloudWatche Logger Implementation.

--- AWSSolution/AWSCloudWatchWebApiCore/Controllers/CWController.cs
+++ AWSSolution/AWSCloudWatchWebApiCore/Controllers/CWController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
 
 namespace AWSCloudWatchWebApiCore.Controllers
 {
@@ -11,11 +13,32 @@ namespace AWSCloudWatchWebApiCore.Controllers
     [ApiController]
     public class CWController : ControllerBase
     {
+        private readonly ILogger<Controller> _logger;
 
-
-        public CWController()
+        public CWController(ILogger<Controller> logger)
         {
+            _logger = logger;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult> LogTest()
+        {
+            _logger.LogInformation("This is an information log.");
+            _logger.LogInformation("This is an warning log.");
+
+            string criticalMessage = "This is a critical message";
+            _logger.LogInformation("Critical Message: {0}", criticalMessage);
+
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error ocurred");
+            }
+
+            return Ok("All ok!");
         }
     }
 }
--- AWSSolution/AWSCloudWatchWebApiCore/Startup.cs
+++ AWSSolution/AWSCloudWatchWebApiCore/Startup.cs
@@ -26,15 +26,22 @@ namespace AWSCloudWatchWebApiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var config = Configuration.GetAWSLoggingConfigSection();
+            loggerFactory.AddAWSProvider(config);
+
+            var logger = loggerFactory.CreateLogger("Startup");
+            logger.LogWarning("warning from configure");
 
             app.UseHttpsRedirection();
 
